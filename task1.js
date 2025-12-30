45049b74cffc904d0b913350ec069875ecccfd0f
Muhamad Wildan Nursyamsi Abdullah Al-Harist <125271650+AbiPasundan@users.noreply.github.com>
2025-12-30 16:11:52 +0700

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/create-hello-world-function/
+// create-hello-world-function
+
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World"
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
